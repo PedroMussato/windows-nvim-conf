973a0b7abe3535be6907f83f62e5b91157e1378f
Pedro Renan Mussato <pedro.mussato@ultrapar.com.br>
2024-06-17 15:50:04 -0300

    rose-pine

---
+++ after/plugin/treesitter.lua
@@ -0,0 +1,12 @@
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = { "python", "rust", "javascript", "lua", "c", "vim", "vimdoc", "query" },
+
+  sync_install = false,
+
+  auto_install = true,
+  highlight = {
+    enable = true,
+
+    additional_vim_regex_highlighting = false,
+  },
+}
